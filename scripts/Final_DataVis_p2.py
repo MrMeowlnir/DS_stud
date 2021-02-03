cbc3368bb6ea36d20cecbb437fb99a252ebc4ed0
MrMeowlnir <dmsdens@gmail.com>
2021-02-04 01:24:51 +0300

    bit changes in ds stud

--- scripts/Final_DataVis_p2.py
+++ scripts/Final_DataVis_p2.py
@@ -8,13 +8,13 @@ import matplotlib.pyplot as plt
 # II. Choropleth map
 # II.1. read_csv
 
-CrimesFrame = pandas.read_csv('data/Police_Department_Incidents_-_Previous_Year__2016_.csv')
+CrimesFrame = pandas.read_csv('data\Police_Department_Incidents_-_Previous_Year__2016_.csv')
 print(CrimesFrame.columns)
 print(CrimesFrame[["PdDistrict", "Location"]].head())
 
 GroupCrimes = CrimesFrame.groupby("PdDistrict").agg({"Location": "nunique"})
 
-print(GroupCrimes)
+print(GroupCrimes.columns)
 # CrimesFrame = CrimesFrame[["Neighborhood", "Count"]]
 # print(CrimesFrame)
 
